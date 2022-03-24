8cedc81b5417f348b17a45b85224294c9e9a1aa5
PabloValles <valles.palblo@hotmail.com>
2022-03-24 19:08:58 -0300

    features(update: app.js)

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const path = require("path");
 
 const app = express();
 const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath));
 
 app.listen(3030, () => console.log("Server runing in port 3030"));
-app.get("/", (req, res) => 
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./views/index.html"))
 );
 
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/login.html"))
+);
+
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/register.html"))
+);
