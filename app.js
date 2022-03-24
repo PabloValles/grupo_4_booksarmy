093fecb8424faba8a361bccc9fbaf2093b6be946
PabloValles <valles.palblo@hotmail.com>
2022-03-24 17:38:54 -0300

    Servidor corriendo

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const express = require("express");
-console.log(express);
+const path = require("path");
+
+const app = express();
+
+app.listen(3030, () => console.log("Server runing in port 3030"));
