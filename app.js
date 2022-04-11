bb54d0b32d34cf6ca70a911c89ce0141e32f14f7
PabloValles <valles.pablo@hotmail.com>
2022-04-11 16:17:13 -0300

    (hotfix) app.js

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
+const app = express();
+
 const path = require("path");
 const { allowedNodeEnvironmentFlags } = require("process");
+
+/*======> Requerimos nuestros routers <======*/
 const mainRouter = require("./routers/main");
 const productsRouter = require("./routers/products");
 
+/*======> Declaramos el puerto donde se ejecutará nuestra app <======*/
 const PORT = process.env.PORT || 3000;
-const app = express();
+
+/*======> Utlizaremos ejs como motor de vistas <======*/
+app.set("view engine", "ejs");
+
+/*======> Definimos los archivos que serán estáticos <======*/
 const publicPath = path.resolve(__dirname, "./public");
-
-app.set( 'view engine', 'ejs');
-
-app.listen(PORT, () => console.log(`Server runing in port ${PORT}`));
-
 app.use(express.static(publicPath));
 
+/*======> Declaramos las rutas <======*/
 app.use("/", mainRouter);
-
 app.use("/products", productsRouter);
+
+/*======> Iniciamos nuestra app <======*/
+app.listen(PORT, () => console.log(`Server runing in port ${PORT}`));
